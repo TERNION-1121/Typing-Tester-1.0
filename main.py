a956e8949c87c8dc3f1bdfe6a89a23abaa811a37
Vikrant Singh Bhadouriya <97667653+TERNION-1121@users.noreply.github.com>
2022-06-17 15:02:32 +0530

    Optimized main.py and Removed timer.py
    Adding docstring to the typing_test() function, optimizing it and removing timer.py as it not further needed.

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import time as t
 
-print("Type the sentence given below:\n")
-sentence = '''Hey there this is a program to test your typing speed in words per minute. I hope you would like it.\n'''
-print(sentence)
+def typing_test():
+    '''This function prints the sentence(pre-defined by the user), then starts the timer as soon as enter is pressed.
+    The timer stops as soon as the enter is pressed after typing.
+    At last it displays the typing speed(WPM) and the accuracy(%)'''
+    print("Type the sentence given below:\n")
+    sentence = '''Hey there this is a program to test your typing speed in words per minute. I hope you would like it.'''
+    print(f"\"{sentence}\"")
+    print()
 
-input("Please press enter to start the timer and start typing.\n")
-start_time = t.time()
-words = input()
-end_time = t.time()
+    input("Please press Enter to start the timer and start typing.\n")
+    start_time = t.time()
+    words = input()
+    end_time = t.time()
 
-time_lapsed = end_time - start_time
-minutes = time_lapsed / 60
+    time_lapsed = end_time - start_time
+    minutes = time_lapsed / 60
 
-characters = len(words)
+    characters = len(words)
 
-wpm = (characters/5) // minutes
-print(f"\n\nYour Typing speed is {wpm} WPM")
+    wpm = (characters/5) // minutes
+    print(f"\n\n\t\t\t\tYour Typing speed is {wpm} WPM")
 
-# print(f"Time taken in seconds: {time_lapsed}\nCharacters in the sentence: {len(words)}")
+    sen_lst = sentence.split(' ')
+    words_lst = words.split(' ')
+    errors = 0
+
+    for i in range(len(sen_lst)):
+        if words_lst[i] != sen_lst[i]:
+            errors+=1
+
+    correct_chars = characters - errors
+    accuracy = (correct_chars / characters) * 100
+
+    print("\t\t\t\tYour Accuracy is %.2f" % accuracy, "%")
+    # print(f"Time taken in seconds: {time_lapsed}\nCharacters in the sentence: {len(words)}")
+    # print(f"Characters in total {characters}\nCorrect Characters {correct_chars}\nErrors {errors}")
+
+typing_test()
--- timer.py
+++
@@ -1,8 +0,0 @@
-import time as t
-
-input("Press Enter to start")
-start_time = t.time()
-input("Press Enter to stop")
-end_time = t.time()
-time_lapsed = end_time - start_time
-print(time_lapsed)
