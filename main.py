729803141415b083388f22297585736c0cde4f6d
Vikrant Singh Bhadouriya <97667653+TERNION-1121@users.noreply.github.com>
2022-06-17 18:42:26 +0530

    Final Changes and Commenting
    Some final optimizations and commenting the code.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import time as t
 
 def typing_test():
-    '''This function prints the sentence(pre-defined by the user), then starts the timer as soon as enter is pressed.
+
+    '''This function prints the sentence(pre-defined by the user in the program), then starts the timer as soon as enter is pressed.
     The timer stops as soon as the enter is pressed after typing.
-    At last it displays the typing speed(WPM) and the accuracy(%)'''
+    At last it displays the typing speed(WPM) and typing accuracy(%)'''
+
     print("Type the sentence given below:\n")
     sentence = '''Hey there this is a program to test your typing speed in words per minute. I hope you would like it.'''
     print(f"\"{sentence}\"")
@@ -18,23 +20,23 @@ def typing_test():
     minutes = time_lapsed / 60
 
     characters = len(words)
-
-    wpm = (characters/5) // minutes
-    print(f"\n\n\t\t\t\tYour Typing speed is {wpm} WPM")
+    wpm = (characters / 5) // minutes # each word would be 5 keystrokes
 
     sen_lst = sentence.split(' ')
     words_lst = words.split(' ')
+    words_entered = len(words_lst)
     errors = 0
 
     for i in range(len(sen_lst)):
         if words_lst[i] != sen_lst[i]:
             errors+=1
 
-    correct_chars = characters - errors
-    accuracy = (correct_chars / characters) * 100
+    correct_words =  words_entered - errors
+    accuracy = (correct_words / words_entered) * 100 # typing accuracy here is taken as correct words / total words
 
+    print(f"\t\t\t\tYour Typing speed is {wpm} WPM")
     print("\t\t\t\tYour Accuracy is %.2f" % accuracy, "%")
-    # print(f"Time taken in seconds: {time_lapsed}\nCharacters in the sentence: {len(words)}")
-    # print(f"Characters in total {characters}\nCorrect Characters {correct_chars}\nErrors {errors}")
+    # print(f"Time taken in seconds: {time_lapsed}\nCharacters in the sentence: {len(words)}") # extra information to confirm the typing speed
+    # print(f"Words in total {words_entered}\nCorrect Words {correct_words}\nErrors {errors}") # extra information to confirm the typing accuracy
 
 typing_test()
